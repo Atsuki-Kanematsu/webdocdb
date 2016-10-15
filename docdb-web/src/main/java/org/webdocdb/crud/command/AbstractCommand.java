e52707288252d48ce0da061bafff48e6f87755f0
Taka <Taka@Taka-PC>
2016-10-15 17:47:25 +0900

    update 10/15

--- docdb-web/src/main/java/org/webdocdb/crud/command/AbstractCommand.java
+++ docdb-web/src/main/java/org/webdocdb/crud/command/AbstractCommand.java
@@ -8,13 +8,23 @@ public class AbstractCommand {
 		APPEND
 	}
 	
+	private static final String LINE_SEPARATOR = "\n";
+	
 	private String command;
 	private String[] args;
 	private RedirectMode redirectMode;
 	private String redirectPath;
 	
 	public AbstractCommand(String commandLine) {
-		commandLine = cutAndParseRedirect(commandLine);
+		commandLine = cutAndParseRedirect(commandLine.trim());
+		int pos = commandLine.indexOf(" ");
+		if (pos == -1) {
+			command = commandLine;
+			args = new String[0];
+		} else {
+			command = commandLine.substring(0, pos);
+			args = command.substring(pos + 1).split(" ");
+		}
 	}
 	
 	private String cutAndParseRedirect(String commandLine) {
@@ -24,10 +34,37 @@ public class AbstractCommand {
 			return commandLine;
 		}
 		String redirectText = commandLine.substring(pos);
-		redirectMode = RedirectMode.WRITE;
 		if (redirectText.startsWith(">>")) {
 			redirectMode = RedirectMode.APPEND;
+			redirectPath = redirectText.substring(2).trim();
+		} else {
+			redirectMode = RedirectMode.WRITE;
+			redirectPath = redirectText.substring(1).trim();
 		}
-		return commandLine.substring(0, pos);
+		return commandLine.substring(0, pos).trim();
+	}
+
+	public String getCommand() {
+		return command;
+	}
+
+	public String[] getArgs() {
+		return args;
+	}
+
+	public RedirectMode getRedirectMode() {
+		return redirectMode;
+	}
+
+	public String getRedirectPath() {
+		return redirectPath;
+	}
+	
+	protected void printConsole(String text) {
+		System.out.print(text);
+	}
+	
+	protected void printConsoleLine(String log) {
+		printConsole(log + LINE_SEPARATOR);
 	}
 }
